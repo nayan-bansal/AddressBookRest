53b8cc8963ac8e40139a62fcaee114203df5f5f9
nayan-bansal <nayan.7991@gmail.com>
2020-11-07 14:26:03 +0530

    Added: New Contact to the Server

--- AddressBookRestMain.java
+++ AddressBookRestMain.java
@@ -1,5 +1,34 @@
 package addresbookrest.address;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBookRestMain {
 
+	private List<Contact> contactList;
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
+	}
+
+	public AddressBookRestMain() {
+	}
+
+	public AddressBookRestMain(List<Contact> contactList) {
+		this();
+		this.contactList = new ArrayList<>(contactList); // Use new memory not the same as provided by client to avoid confusion
+
+	}
+
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.REST_IO))
+			return contactList.size();
+		return 0;
+	}
+
+	public void addContactToAddressBook(Contact contactData, IOService restIo) {
+		contactList.add(contactData);
+		System.out.println(contactList.size());
+	}
+	
 }
