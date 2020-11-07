4f42bbb34c99b780593568e58a619079966d9755
nayan-bansal <nayan.7991@gmail.com>
2020-11-07 17:02:48 +0530

    Added: Updated Contact in the Database

--- AddressBookRestTest.java
+++ AddressBookRestTest.java
@@ -15,6 +15,7 @@ import addresbookrest.address.Contact;
 
 public class AddressBookRestTest {
 
+	
 	private static Logger log = Logger.getLogger(AddressBookRestTest.class.getName());
 	private static AddressBookRestMain addressBookService = null;
 
@@ -23,6 +24,24 @@ public class AddressBookRestTest {
 		log.info("Contact entries in JSON Server :\n" + response.asString());
 		Contact[] arrayOfEmps = new Gson().fromJson(response.asString(), Contact[].class);
 		return arrayOfEmps;
+	}
+
+	private Response addContactToJSONServer(Contact contactData) {
+		String contactJSON = new Gson().toJson(contactData);
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.body(contactJSON);
+		return request.post("/contacts");
+	}
+
+	private int updateSalaryToJSONServer(Contact contactData) {
+		String contactJSON = new Gson().toJson(contactData);
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.body(contactJSON);
+		Response response = request.put("/contacts/" + contactData.id);
+
+		return response.getStatusCode();
 	}
 
 	@Before
@@ -36,14 +55,24 @@ public class AddressBookRestTest {
 		Contact[] arrayOfContacts = getContactList();
 		addressBookService = new AddressBookRestMain(Arrays.asList(arrayOfContacts));
 		long entries = addressBookService.countEntries(IOService.REST_IO);
-		Assert.assertEquals(6, entries);
+		Assert.assertEquals(10, entries);
 	}
-	
+
+	@Test
+	public void givenEmployeeData_WhenUpdatedInJSONServer_ShouldSyncWithList() {
+		Contact[] arrayOfContacts = getContactList();
+		addressBookService = new AddressBookRestMain(Arrays.asList(arrayOfContacts));
+		addressBookService.updateContactSalary("DevS", 4000000.00, IOService.REST_IO);
+		Contact contactData = addressBookService.getEmployeeData("DevS");
+		int statusCode = updateSalaryToJSONServer(contactData);
+		Assert.assertEquals(200, statusCode);
+	}
+
 	@Test
 	public void givenNewEmployee_WhenAddedInJsonServer_ShouldMatchResponseAndCount() {
 		Contact[] arrayOfEmps = getContactList();
 		addressBookService = new AddressBookRestMain(Arrays.asList(arrayOfEmps));
-		Contact contactData = new Contact(0, "Mark", "M", 2000000, LocalDate.now());
+		Contact contactData = new Contact(0, "Mark ZukerBerg", "M", 3000000, LocalDate.now());
 		Response response = addContactToJSONServer(contactData);
 		int statusCode = response.getStatusCode();
 		Assert.assertEquals(201, statusCode);
@@ -53,7 +82,7 @@ public class AddressBookRestTest {
 		long entries = addressBookService.countEntries(IOService.REST_IO);
 		Assert.assertEquals(7, entries);
 	}
-	
+
 	@Test
 	public void givenListOfNewContacts_WhenAdded__ShouldMatchContactsCount() {
 		Contact[] arrayOfEmps = getContactList();
